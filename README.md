481e971aaf337bc001e02ab832d9d603c325f0f3
Cody Winters <cody.winters@covalents.app>
2024-02-16 22:14:45 -0700

    Created elevator simulator.

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# elevator
-An Elevator Simulator
+# Elevator Simulator
+
+## Background
+Elevator Simulator is a simple console game that allows the user to experience the thrill of riding an elevator without leaving the comfort of their desk.
+
+## Requirements
+
+### Functional Requirements
+
+* The simulator will be an executable script written in TypeScript
+* The simulator will accept an integer input for the starting floor.
+* The simulator will accept a character input for the initial direction of travel, up or down (u or d).
+* The simulator will accept an input of an array of ints representing the floors to visit.
+* The travel time between floors will be a constant 10s.
+* The output of a run will be the total travel time and the floors visited in the order they were visited.
+
+### Non-functional Requirements
+
+* The elevator will stop on intermediate floors on the way instead of visiting the floors in the exact order given. Given a starting floor of 12, a direction of down, and a list of floors to visit [2, 9, 1, 32] the elevator would visit the floors in the order [9, 2, 1, 32].
+* Given a direction, the elevator will move in that direction first, even if another given floor in the opposite direction is closer to the starting position.
+* If the starting floor for a run is different than the ending floor of the previous run, there will be travel time to arrive at the new starting floor.
+
+## Installing the simulator
+
+### Prerequisites
+* Node.js and npm must be installed on your system. See https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
+* You must install the typescript compiler using the command (sudo access may be required):
+    * `npm install -g typescript` 
+
+### Installation
+1. Clone the package into your local repository
+    * Https: `git clone https://github.com/wintersca/elevator.git`
+    * Github CLI: `gh repo clone wintersca/elevator`
+2. Navigate into the Elevator directory
+    * `cd elevator`
+3. Install required pacakges and build the script
+    * `npm run build`
+4. Run the Script
+    * `npm run start`
+
+### Troubleshooting
+If there is an error, you can clean the project and uninstall all packages by running `npm run clean`. Then run the build command again to reinstall all required packages.
+
+## Running the Elevator Simulator
+After following the installation instructions above, you should be able to run the simulator and travel between floors at your leisure. To start a session run the command: 
+
+`npm run start`
+
+The rules below will be shown as soon as you start the simulator.
+
+### Rules
+
+1. When starting a run, you are required to enter a starting floor. Which represents the floor you are on when you call the elevator.
+
+2. For all runs, you will need to input a direction `u` or `d` representing up and down respectively. This represents pressing the call button on the elevator.
+
+3. All floors supplied in the array will be assumed to have been pressed at the same time, so the elevator will travel in the direction specified first, stopping at all floors in that direction, and then travel to any floors that were in the opposite direction of the call.
+    * For example given a starting floor of 12, a direction of down, and a list of floors to visit [1, 9, 2, 34] the elevator will travel down and stop at floors 9, 2, 1 in that order before traveling to floor 34.
+
+4. Fractional floors (e.g. 3.5) are not allowed.
+
+5. After the final floor is reached, the total travel time, the list of floors visited in the order they were visited, and the current floor will be shown.
+
+6. Runs don't need to start at the floor the last run ended on.
+    * After the first run, pressing the call button (e.g. entering a starting floor and `u` or `d`) will call the elevator from the end position of the last run. Travel time will be added to the total run time representing the elevator traveling from its previous position to the new starting floor.
+
+
---
+++ src/elevator.ts
@@ -0,0 +1,277 @@
+import * as readline from 'readline';
+import { colors, travelTimeConst } from './constants';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+var firstRun: boolean = true;
+var startingFloor: number = 1;
+var currentFloor: number = 1;
+var direction = '';
+
+console.log(`${colors.bg.black}${colors.fg.yellow}` +
+'#######   ###                                  ##                      \n' +
+'##         ##                                  ##                      \n' +
+'##         ##      #####   ### ###   ######  ######    #####   ## ###  \n' +
+'#####      ##     ##   ##   ## ##   ##   ##    ##     ##   ##  ###     \n' +
+'##         ##     #######   ## ##   ##   ##    ##     ##   ##  ##      \n' +
+'##         ##     ##         ###    ##  ###    ##     ##   ##  ##      \n' +
+'#######   ####     #####     ###     ### ##     ###    #####   ##      \n' +
+'                                                                       \n' +
+' #####     ##                                                          \n' +
+'##   ##                                                                \n' +
+'##       ####     ### ##                                               \n' +
+' #####     ##     ## # ##                                              \n' +
+'     ##    ##     ## # ##                                              \n' +
+'##   ##    ##     ## # ##                                              \n' +
+' #####   ######   ##   ##                                              \n' +
+'\n' +
+'Welcome to Elevator Simulator, a new and exciting game.\n' +
+`Hold on to your seat as you experience the thrill of riding an elevator!\n\n`)
+
+printRules('./README.md');
+
+getStartingFloor();
+
+/**
+ * Prompts the user for the starting floor for the run. On the first run, the 
+ * starting floor will also be used as the current floor for determining total
+ * travel time. On subsequent runs of the same session, the current floor will
+ * be the ending floor from the previous run.
+ */
+export function getStartingFloor(): void {
+    const reader = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    reader.setPrompt('Please enter your starting floor. (int: e.g. 1): ');
+    reader.prompt();
+    reader.on('line', (input) => {
+        if (!isInt(input)) {
+            console.log("The input is not an integer.")
+            reader.close();
+            getStartingFloor();
+        }
+        else {
+            startingFloor = Number(input);
+            if (firstRun) {
+                currentFloor = startingFloor;
+                firstRun = false;
+            }
+            reader.close();
+            getDirrection();
+        }
+    });
+}
+
+/**
+ * Prompts the user for the direction that the elevator will travel first. If the input
+ * provided is not acceptable, then the user will be shown an error message and prompted
+ * for the dirrection again. Acceptable input is 'u' or 'up for up and 'd' or 'down' for 
+ * down.
+ */
+export function getDirrection(): void {
+    const reader = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    reader.setPrompt('Which direction are you going? (char: u or d): ');
+    reader.prompt();
+    reader.on('line', (input) => {
+        const dir = input.toLowerCase();
+        if (dir == 'u' || dir == 'up') {
+            direction = 'u';
+            reader.close();
+            getFloors();
+        }   
+        else if (dir == 'd' || dir == 'down') {
+            direction = 'd'
+            reader.close();
+            getFloors();
+        }   
+        else {
+            console.log('Please choose either u or d')
+            reader.close();
+            getDirrection();
+        }    
+    });
+}
+
+/**
+ * Prompts the user to input an array of integers which represent the floors that
+ * will be visited by the elevator on this run. If the input is not an array, or 
+ * the array does not contain the correct type, an error message will be shown and 
+ * the user will be prompted for the information again.
+ */
+export function getFloors(): void {
+    const reader = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    var validInput = true;
+    reader.setPrompt('What floors are you visiting? (int[]: e.g. [1, 3, 15]): ');
+    reader.prompt();
+    reader.on('line', (input) => {
+        try {
+            const floors = JSON.parse(input);
+            if (Array.isArray(floors)) {
+                for (const floor of floors) {
+                    if (!isInt(floor)) {
+                        console.log(`Input value: ${floor} is not an int. Please only enter integer values in your floors array.`)
+                        validInput = false;
+                        break;
+                    }
+                }
+                if (validInput) {
+                    const floorNumbers: number[] = floors.map(Number);
+                    reader.close();
+                    travel(floorNumbers);
+                }
+                else {
+                    reader.close();
+                    getFloors();
+                }
+            } 
+            else {
+              console.log('The input is not an array.');
+              reader.close();
+              getFloors();
+            }
+        } 
+        catch (e) {
+            console.log('Invalid input. Please enter a valid array.');
+            reader.close();
+            getFloors();
+        }  
+    });
+}
+
+/**
+ * Organizes the supplied floors to visit into two arrays of floors which are 
+ * above and below the starting floor. Each floor will only be added to it's 
+ * corresponding array once. Calls the goingUp() or goingDown() methods and
+ * reports the total travel time and floors visited to the user.
+ * 
+ * @param floors 
+ */
+export function travel(floors: number[]) {
+    var floorsAbove: number[] = [];
+    var floorsBelow: number[] = [];
+    var floorsVisited: number[] = [currentFloor];
+    var travelTime: number = 0;
+    if (currentFloor !== startingFloor) {
+        travelTime += Math.abs(startingFloor - currentFloor) * travelTimeConst;
+        floorsVisited.push(startingFloor);
+        currentFloor = startingFloor;
+    }
+    
+    console.log("*Ding* Here we go!");
+
+    floors.sort((a, b) => a - b);
+
+    for (const floor of floors) {
+        if (floor > currentFloor && !floorsAbove.includes(floor)){
+            floorsAbove.push(floor);
+        }
+        else if (floor < currentFloor && !floorsBelow.includes(floor)) {
+            floorsBelow.push(floor);
+        }
+    }
+
+    if (direction == 'u') {
+        travelTime += goingUp(floorsAbove, floorsVisited);
+        travelTime += goingDown(floorsBelow, floorsVisited);
+    }
+    else if (direction == 'd') {
+        travelTime += goingDown(floorsBelow, floorsVisited);
+        travelTime += goingUp(floorsAbove, floorsVisited);
+    }
+    
+    console.log(`Total Travel Time: ${travelTime}\nFloors Visited: ${floorsVisited}\nCurrent Floor: ${currentFloor}`);
+    console.log('\n\n What fun, ready for another ride?\n\n');
+    getStartingFloor();
+}
+
+/**
+ * Calculates and returns the travel time for the elevator when the chosen direction
+ * was up. Floors that are above the starting floor will be visited first in 
+ * ascending order.
+ * 
+ * @param floorsAbove 
+ * @param floorsVisited 
+ * @returns travelTime in seconds.
+ */
+export function goingUp(floorsAbove: number[], floorsVisited: number[]): number {
+    var travelTime = 0;
+    for (let i = 0; i < floorsAbove.length; i++){
+        const elapsedTime = (floorsAbove[i] - currentFloor) * travelTimeConst;
+        travelTime += elapsedTime;
+        floorsVisited.push(floorsAbove[i]);
+        currentFloor = floorsAbove[i];
+    }
+    return travelTime;
+}
+
+/**
+ * Calculates and returns the travel time for the elevator when the chosen direction
+ * was down. Floors that are below the starting floor will be visited first in 
+ * descending order.
+ * 
+ * @param floorsBelow 
+ * @param floorsVisited 
+ * @returns travelTime in seconds.
+ */
+export function goingDown(floorsBelow: number[], floorsVisited: number[]): number {
+    var travelTime = 0;
+    for (let i = floorsBelow.length - 1; i >= 0; i--){
+        const elapsedTime = (currentFloor - floorsBelow[i]) * travelTimeConst;
+        travelTime += elapsedTime;
+        floorsVisited.push(floorsBelow[i]);
+        currentFloor = floorsBelow[i];
+    }
+    return travelTime;
+}
+
+/**
+ * Returns true if a given string input can be cast to a number and if that number is an integer.
+ * 
+ * @param input 
+ * @returns 
+ */
+export function isInt(input: string): boolean {
+    if (input == undefined || input == '') {
+        return false;
+    }
+
+    const num = Number(input);
+
+    if (isNaN(num)) {
+        return false;
+    } else {
+        return num % 1 == 0 ? true : false
+    }
+}
+
+
+/**
+ * Prints the rules section of the README.md file at the start of the simulator.
+ * 
+ * @param filePath 
+ */
+export function printRules(filePath: string): void {
+    try {
+        const path = resolve(filePath);
+        const data = readFileSync(path, { encoding: 'utf-8' });
+        const start = data.indexOf('### Rules');
+
+        if (start !== -1) {
+            const rules = data.substring(start);
+            console.log(rules);
+        }
+        else {
+            console.log("The Rules heading was not found in the README file.");
+        }
+    }
+    catch (error) {
+        console.error('Error reading the README file:', error);
+    }
+}
